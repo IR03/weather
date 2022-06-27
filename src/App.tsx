2f986a3ab09506f2e505add83f901e3ca01cb70e
unknown <iftikharsakib@gmail.com>
2022-06-27 11:49:25 +0600

    unnecessary things removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import Search from './Pages/Search';
 import CountryInformation from './Pages/CountryInformation';

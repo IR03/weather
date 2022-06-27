0722e249a48334c886911d690abb2c8a8781c506
unknown <iftikharsakib@gmail.com>
2022-06-27 11:58:02 +0600

    unnecessary things removed

--- src/Pages/CountryInformation.tsx
+++ src/Pages/CountryInformation.tsx
@@ -59,7 +59,7 @@ const CountryInformation: React.FC = () => {
 
     useEffect(() => {
         callCountry();
-    }, [country]);
+    }, []);
 
     const callCountry = async () => {
         try {
